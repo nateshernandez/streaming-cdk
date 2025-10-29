bb9784f5bffd22b5a5ad3723015725e94db7c76c
Nate Hernandez <nate.scott.hernandez@gmail.com>
2025-10-29 13:56:43 -0700

    Fixes code pipeline github connection.

--- src/StreamingCdk/StreamingCdkPipelineStack.cs
+++ src/StreamingCdk/StreamingCdkPipelineStack.cs
@@ -18,7 +18,10 @@ public class StreamingCdkPipelineStack : Stack
             PipelineName = pipelineId,
             Synth = new ShellStep("Synth", new ShellStepProps
             {
-                Input = CodePipelineSource.GitHub("OWNER/REPO", "main"),
+                Input = CodePipelineSource.Connection("nateshernandez/streaming-cdk", "main", new ConnectionSourceOptions
+                {
+                    ConnectionArn = "arn:aws:codeconnections:us-east-2:975050288287:connection/0c61784b-964f-4b34-a2f5-4221e21dc11c"
+                }),
                 Commands = new string[] { "npm install -g aws-cdk", "cdk synth" }
             })
         });
