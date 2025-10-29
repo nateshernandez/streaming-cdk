981c2eb090fb38863e39be00e5d29aa645ee424a
Nate Hernandez <nate.scott.hernandez@gmail.com>
2025-10-29 14:25:40 -0700

    Updates build environment for code pipeline.

--- src/StreamingCdk/StreamingCdkPipelineStack.cs
+++ src/StreamingCdk/StreamingCdkPipelineStack.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon.CDK;
 using Amazon.CDK.Pipelines;
+using Amazon.CDK.AWS.CodeBuild;
 using Constructs;
 
 namespace StreamingCdk;
@@ -22,8 +23,21 @@ public class StreamingCdkPipelineStack : Stack
                 {
                     ConnectionArn = "arn:aws:codeconnections:us-east-2:975050288287:connection/0c61784b-964f-4b34-a2f5-4221e21dc11c"
                 }),
-                Commands = new string[] { "npm install -g aws-cdk", "cdk synth" }
-            })
+                Commands = new string[] { "npm install -g aws-cdk", "cdk synth" },
+                InstallCommands = new string[]
+                {
+                    "npm install -g aws-cdk"
+                },
+                PrimaryOutputDirectory = "cdk.out"
+            }),
+            CodeBuildDefaults = new CodeBuildOptions
+            {
+                BuildEnvironment = new BuildEnvironment
+                {
+                    BuildImage = LinuxBuildImage.STANDARD_7_0,
+                    ComputeType = ComputeType.SMALL
+                }
+            }
         });
 
         pipeline.AddStage(new StreamingCdkStage(this, nameof(StreamingCdkStage), new StageProps
